c9466e3687f6f6cb4372946e4f859a391e97c6a4
Nathan Ansel <nathan3o4@gmail.com>
2016-09-18 11:34:40 -0500

    Solution 8

--- Swift Playground/Project Euler Solutions.playground/Contents.swift
+++ Swift Playground/Project Euler Solutions.playground/Contents.swift
@@ -1,5 +1,24 @@
 import PlaygroundSupport
 
+// Extensions to make my life easier
+extension String {
+    
+    private subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = index(startIndex, offsetBy: r.lowerBound)
+        let end = index(start, offsetBy: r.upperBound - r.lowerBound)
+        return self[Range(start ..< end)]
+    }
+}
+
+
 enum Euler {
     // Project 1: Multiples of 3 and 5
     static func project1() -> String {
@@ -146,7 +165,25 @@ enum Euler {
         }
         return "Project 7 failure"
     }
+    
+    // Project 8: Largest product in a series
+    static func project8() -> String {
+        let digitToProcess = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
+        var maxProduct = 0
+        var numbersToMultiply = [Int]()
+        var maxNumbers = [Int]()
+        var currentProduct = 1
+        for i in 12..<digitToProcess.characters.count {
+            currentProduct = 1
+            for j in (i - 12)...i {
+                currentProduct *= Int(digitToProcess[j]) ?? 0
+            }
+            if currentProduct > maxProduct {
+                maxProduct = currentProduct
+            }
+        }
+        return "\(maxProduct)"
+    }
 }
 
-print(Euler.project7())
-
+print(Euler.project8())
